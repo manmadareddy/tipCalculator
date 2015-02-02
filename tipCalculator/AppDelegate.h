04bccba9727da4e425b5975f4be06233b7fc9fc2
Manmada Reddy Nallagonda <manmada@kabobride-lm.corp.yahoo.com>
2015-02-02 15:46:25 -0800

    Initial Commit

---
+++ tipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipCalculator
+//
+//  Created by Manmada Reddy Nallagonda on 2/2/15.
+//  Copyright (c) 2015 Manmada Reddy Nallagonda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipCalculator
+//
+//  Created by Manmada Reddy Nallagonda on 2/2/15.
+//  Copyright (c) 2015 Manmada Reddy Nallagonda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
